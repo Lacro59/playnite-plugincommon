006af6b081b852076d828f9499738e85970d7830
Lacro59 <lacro59@gmail.com>
2020-05-25 11:04:03 +0200

    Add CumulErrors.

---
+++ PluginCommon/CumulsErrors.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SuccessStory.Common
+{
+    class CumulErrors
+    {
+        private List<string> ListMessage = new List<string>();
+
+        public void Add(string Message)
+        {
+            if (!ListMessage.Exists(x => x == Message))
+            {
+                ListMessage.Add(Message);
+            }
+        }
+
+        public string Get()
+        {
+            string Result = "";
+            for (int i = 0; i < ListMessage.Count; i++)
+            {
+                Result += ((i != 0) ? Environment.NewLine : "") + ListMessage[i];
+            }
+            return Result;
+        }
+    }
+}
