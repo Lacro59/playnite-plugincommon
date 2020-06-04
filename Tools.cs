b74b17ef24c7c7cd65f8ec5443e925ee4f7bf456
Lacro59 <lacro59@gmail.com>
2020-06-04 11:43:23 +0200

    Add tools functions.

---
+++ Tools.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace PluginCommon
+{
+    class Tools
+    {
+        // https://stackoverflow.com/a/48735199
+        public static JArray RemoveValue(JArray oldArray, dynamic obj)
+        {
+            List<string> newArray = oldArray.ToObject<List<string>>();
+            newArray.Remove(obj);
+            return JArray.FromObject(newArray);
+        }
+    }
+}
