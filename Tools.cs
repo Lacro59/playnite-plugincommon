cd5458e1ea8f73fd32949ea7113ca8ed9e0de0cc
Lacro59 <lacro59@gmail.com>
2020-06-04 16:56:26 +0200

    Add WarningsDialogs.

--- Tools.cs
+++ Tools.cs
@@ -1,9 +1,11 @@
 ﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace PluginCommon
 {
-    class Tools
+    public class Tools
     {
         // https://stackoverflow.com/a/48735199
         public static JArray RemoveValue(JArray oldArray, dynamic obj)
@@ -11,6 +13,17 @@ namespace PluginCommon
             List<string> newArray = oldArray.ToObject<List<string>>();
             newArray.Remove(obj);
             return JArray.FromObject(newArray);
+        }
+
+        /// <summary>
+        /// Get number week.
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static int WeekOfYearISO8601(DateTime date)
+        {
+            var day = (int)CultureInfo.CurrentCulture.Calendar.GetDayOfWeek(date);
+            return CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(date.AddDays(4 - (day == 0 ? 7 : day)), CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
         }
     }
 }
