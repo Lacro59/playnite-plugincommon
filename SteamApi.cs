0ed50be2747a526eabbd271d1b6ed97792e4d1e1
Lacro59 <lacro59@gmail.com>
2020-07-31 19:40:26 +0200

    Add SteamApi

---
+++ SteamApi.cs
@@ -0,0 +1,130 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Playnite.SDK;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PluginCommon
+{
+    public class SteamApi
+    {
+        private static readonly ILogger logger = LogManager.GetLogger();
+
+        private readonly string urlSteamListApp = "https://api.steampowered.com/ISteamApps/GetAppList/v2/";
+
+        private string PluginCachePath;
+
+        private JObject SteamListApp = new JObject();
+
+
+        internal async Task<string> DownloadStringData(string url)
+        {
+            logger.Debug($"PluginCommon - Download {url}");
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    string responseData = await client.GetStringAsync(url).ConfigureAwait(false);
+                    return responseData;
+                }
+            }
+            catch (Exception ex)
+            {
+                Common.LogError(ex, "PluginCommon", "");
+                return null;
+            }
+        }
+
+        public SteamApi(string PluginUserDataPath)
+        {
+            // Class variable
+            PluginCachePath = PluginUserDataPath + "\\cache\\";
+            string PluginCacheFile = PluginCachePath + "\\SteamListApp.json";
+
+            // Load Steam list app
+            try
+            {
+                if (Directory.Exists(PluginCachePath))
+                {
+                    // From cache if it exists
+                    if (File.Exists(PluginCacheFile))
+                    {
+                        // If not expired
+                        if (File.GetLastWriteTime(PluginCacheFile).AddDays(3) > DateTime.Now)
+                        {
+                            logger.Debug("PluginCommon - GetSteamAppListFromCache");
+                            SteamListApp = JObject.Parse(File.ReadAllText(PluginCacheFile));
+                        }
+                        else
+                        {
+                            SteamListApp = GetSteamAppListFromWeb(PluginCacheFile);
+                        }
+                    }
+                    // From web
+                    else
+                    {
+                        SteamListApp = GetSteamAppListFromWeb(PluginCacheFile);
+                    }
+                }
+                else
+                {
+                    Directory.CreateDirectory(PluginCachePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                Common.LogError(ex, "PluginCommon", "Error on load SteamListApp");
+            }
+        }
+
+        private JObject GetSteamAppListFromWeb(string PluginCacheFile)
+        {
+            logger.Debug("PluginCommon - GetSteamAppListFromWeb");
+            string responseData = DownloadStringData(urlSteamListApp).GetAwaiter().GetResult();
+            if (responseData.IsNullOrEmpty() && responseData!= "{\"applist\":{\"apps\":[]}}")
+            {
+                responseData = JsonConvert.SerializeObject(new JObject());
+            }
+            else
+            {
+                File.WriteAllText(PluginCacheFile, responseData);
+            }
+            return JObject.Parse(responseData);
+        }
+
+        public int GetSteamId(string Name, bool IsLoop1 = false, bool IsLoop2 = false)
+        {
+            int SteamId = 0;
+
+            try
+            {
+                foreach (JObject Game in SteamListApp["applist"]["apps"])
+                {
+                    if (((string)Game["name"]).Trim().ToLower() == Name.ToLower())
+                    {
+                        return (int)Game["appid"];
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Common.LogError(ex, "PluginCommon", "Error on GetSteamId for {Name}");
+            }
+
+            if (!IsLoop1)
+            {
+                SteamId = GetSteamId(Name.Replace(":", ""), true);
+                if ((SteamId == 0) && (!IsLoop2))
+                {
+                    SteamId = GetSteamId(Name + "™", true, true);
+                }
+                return SteamId;
+            }
+
+            return SteamId;
+        }
+    }
+}
