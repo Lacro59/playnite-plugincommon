d94cbe900304a5e33a0824a1d660a12ce32688ce
Lacro59 <lacro59@gmail.com>
2020-12-31 11:11:55 +0100

    codes optimizations

--- CommonPluginsResources/Properties/AssemblyInfo.cs
+++ CommonPluginsResources/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("CommonPluginsPlaynite")]
+[assembly: AssemblyTitle("CommonPluginsResources")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CommonPluginsPlaynite")]
+[assembly: AssemblyProduct("CommonPluginsRessources")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("a4c600fb-92f5-4fd6-ad6b-0c0e0cf2ec81")]
+[assembly: Guid("4effe926-9c64-4750-a399-f344710edc09")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
