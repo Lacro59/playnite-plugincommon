6c38d1528c8d9926a1401dc38fadfc7f3cf65807
Lacro59 <lacro59@gmail.com>
2021-01-19 11:48:23 +0100

    optimization in CheckVersion class

--- CommonPluginsShared/CheckVersion.cs
+++ CommonPluginsShared/CheckVersion.cs
@@ -10,12 +10,14 @@ using System.Diagnostics;
 using System.IO;
 using System.Net;
 using YamlDotNet.Serialization;
+using System.Threading.Tasks;
 
 namespace CommonPluginsShared
 {
     public class CheckVersion
     {
         private static ILogger logger = LogManager.GetLogger();
+        private static IResourceProvider resources = new ResourceProvider();
 
         private string PluginName = string.Empty;
         private ExtensionManifest PluginInfo;
@@ -27,68 +29,74 @@ namespace CommonPluginsShared
         private string LastReleaseBody = string.Empty;
 
 
-        public bool Check(string PluginName, string PluginFolder)
+        public void Check(string PluginName, string PluginFolder, IPlayniteAPI PlayniteApi)
         {
-            this.PluginName = PluginName;
-            var deserializer = new DeserializerBuilder().IgnoreUnmatchedProperties().Build();
+            Task.Run(() =>
+            {
+                this.PluginName = PluginName;
+                var deserializer = new DeserializerBuilder().IgnoreUnmatchedProperties().Build();
 
-            var path = Path.Combine(PluginFolder, PlaynitePaths.ExtensionManifestFileName);
-            PluginInfo = ExtensionManifest.FromFile(path);
+                var path = Path.Combine(PluginFolder, PlaynitePaths.ExtensionManifestFileName);
+                PluginInfo = ExtensionManifest.FromFile(path);
 
-            // Get Github info
-            string url = string.Format(urlGithub, PluginName.ToLower());
+                // Get Github info
+                string url = string.Format(urlGithub, PluginName.ToLower());
 
 #if DEBUG
-            logger.Debug($"CommonPluginsShared [Ignored] - Download {url} for {PluginName}");
+                logger.Debug($"CommonPluginsShared [Ignored] - Download {url} for {PluginName}");
 #endif
 
-            string ResultWeb = string.Empty;
-            try
-            {
-                ResultWeb = Web.DownloadStringData(url, WebUserAgentType.Request).GetAwaiter().GetResult();
-            }
-            catch (WebException ex)
-            {
-                logger.Error($"Failed to load from {url} for {PluginName}");
-#if DEBUG
-                Common.LogError(ex, "CommonPluginsShared [Ignored]", $"Failed to load from {url} for {PluginName}");
-#endif
-            }
-
-            LastReleaseUrl = string.Empty;
-            LastReleaseTagName = string.Empty;
-            LastReleaseBody = string.Empty;
-
-            if (!ResultWeb.IsNullOrEmpty())
-            {
+                string ResultWeb = string.Empty;
                 try
                 {
-                    JArray resultObj = JArray.Parse(ResultWeb);
-                    if (resultObj[0]["html_url"] != null)
-                    {
-                        LastReleaseUrl = (string)resultObj[0]["html_url"];
-                        LastReleaseTagName = (string)resultObj[0]["tag_name"];
-                        LastReleaseBody = (string)resultObj[0]["body"];
-                    }
-
-                    logger.Info($"CommonPluginsShared - {PluginName} - Find {LastReleaseTagName} - Actual v{PluginInfo.Version}");
+                    ResultWeb = Web.DownloadStringData(url, WebUserAgentType.Request).GetAwaiter().GetResult();
                 }
-                catch (Exception ex)
+                catch (WebException ex)
                 {
-                    Common.LogError(ex, "CommonPluginsShared", $"Failed to parse Github response for {PluginName} - {ResultWeb}");
-                    return false;
+                    logger.Error($"Failed to load from {url} for {PluginName}");
+#if DEBUG
+                    Common.LogError(ex, "CommonPluginsShared [Ignored]", $"Failed to load from {url} for {PluginName}");
+#endif
                 }
-            }
-            else
-            {
-                logger.Warn($"CommonPluginsShared - No Data from {url} for {PluginName}");
-            }
 
-            //Check actual vs Github
-            return (!LastReleaseTagName.IsNullOrEmpty() && LastReleaseTagName != "v" + PluginInfo.Version);
+                LastReleaseUrl = string.Empty;
+                LastReleaseTagName = string.Empty;
+                LastReleaseBody = string.Empty;
+
+                if (!ResultWeb.IsNullOrEmpty())
+                {
+                    try
+                    {
+                        JArray resultObj = JArray.Parse(ResultWeb);
+                        if (resultObj[0]["html_url"] != null)
+                        {
+                            LastReleaseUrl = (string)resultObj[0]["html_url"];
+                            LastReleaseTagName = (string)resultObj[0]["tag_name"];
+                            LastReleaseBody = (string)resultObj[0]["body"];
+                        }
+
+                        logger.Info($"CommonPluginsShared - {PluginName} - Find {LastReleaseTagName} - Actual v{PluginInfo.Version}");
+                    }
+                    catch (Exception ex)
+                    {
+                        Common.LogError(ex, "CommonPluginsShared", $"Failed to parse Github response for {PluginName} - {ResultWeb}");
+                        return;
+                    }
+                }
+                else
+                {
+                    logger.Warn($"CommonPluginsShared - No Data from {url} for {PluginName}");
+                }
+
+                //Check actual vs Github
+                if (!LastReleaseTagName.IsNullOrEmpty() && LastReleaseTagName != "v" + PluginInfo.Version)
+                {
+                    ShowNotification(PlayniteApi, "HowLongToBeat - " + resources.GetString("LOCUpdaterWindowTitle"));
+                }
+            });
         }
 
-        public void ShowNotification(IPlayniteAPI PlayniteApi, string Message)
+        private void ShowNotification(IPlayniteAPI PlayniteApi, string Message)
         {
             PlayniteApi.Notifications.Add(new NotificationMessage(
                 $"CheckVersion-{PluginName}",
