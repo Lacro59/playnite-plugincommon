0265e7ed26cd7f60bf648a4e74a4729f3a76e068
Lacro59 <lacro59@gmail.com>
2021-02-03 18:57:27 +0100

    Added OptionsDownloadData

---
+++ CommonPluginsControls/Controls/OptionsDownloadData.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CommonPluginsControls.Controls
+{
+    /// <summary>
+    /// Logique d'interaction pour OptionsDownloadData.xaml
+    /// </summary>
+    public partial class OptionsDownloadData : UserControl
+    {
+        private List<Game> _FilteredGames { get; set; }
+
+
+        public OptionsDownloadData(IPlayniteAPI PlayniteApi)
+        {
+            _FilteredGames = PlayniteApi.Database.Games.ToList();
+
+            InitializeComponent();
+        }
+
+
+        private void PART_BtClose_Click(object sender, RoutedEventArgs e)
+        {
+            ((Window)this.Parent).Close();
+        }
+
+        private void PART_BtDownload_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)PART_AllGames.IsChecked)
+            {
+                
+            }
+
+            if ((bool)PART_GamesRecentlyPlayed.IsChecked)
+            {
+                _FilteredGames = _FilteredGames.Where(x => (DateTime)x.LastActivity >= DateTime.Now.AddMonths(-1)).ToList();
+            }
+
+            if ((bool)PART_GamesRecentlyAdded.IsChecked)
+            {
+                _FilteredGames = _FilteredGames.Where(x => (DateTime)x.Added >= DateTime.Now.AddMonths(-1)).ToList();
+            }
+
+            if ((bool)PART_GamesInstalled.IsChecked)
+            {
+                _FilteredGames = _FilteredGames.Where(x => x.IsInstalled).ToList();
+            }
+
+            if ((bool)PART_GamesNotInstalled.IsChecked)
+            {
+                _FilteredGames = _FilteredGames.Where(x => !x.IsInstalled).ToList();
+            }
+
+            if ((bool)PART_GamesFavorite.IsChecked)
+            {
+                _FilteredGames = _FilteredGames.Where(x => x.Favorite).ToList();
+            }
+
+            ((Window)this.Parent).Close();
+        }
+
+
+        public List<Game> GetFilteredGames()
+        {
+            return _FilteredGames;
+        }
+
+
+
+    }
+}
