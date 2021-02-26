31d1553087a0a7768e846eae745a2c6efa5fc105
LABOURE ROMAIN <rlaboure@tibco.fr>
2021-02-26 10:29:56 +0100

    optimization PluginExtended

--- CommonPluginsShared/PlayniteExtended/PluginExtended.cs
+++ CommonPluginsShared/PlayniteExtended/PluginExtended.cs
@@ -9,8 +9,31 @@ using System.Text;
 
 namespace CommonPluginsShared.PlayniteExtended
 {
-    public abstract class PluginExtended<ISettings, TPluginDatabase> : Plugin
+    public abstract class PluginExtended<ISettings> : PlaynitePlugin<ISettings>
+    {
+        public PluginExtended(IPlayniteAPI api) : base(api)
+        {
+
+        }
+    }
+
+
+    public abstract class PluginExtended<ISettings, TPluginDatabase> : PlaynitePlugin<ISettings>
         where TPluginDatabase : IPluginDatabase
+    {
+        public static TPluginDatabase PluginDatabase { get; set; }
+
+
+        public PluginExtended(IPlayniteAPI api) : base(api)
+        {
+            // Get plugin's database if used
+            PluginDatabase = typeof(TPluginDatabase).CrateInstance<TPluginDatabase>(PlayniteApi, PluginSettings, this.GetPluginUserDataPath());
+            PluginDatabase.InitializeDatabase();
+        }
+    }
+
+
+    public abstract class PlaynitePlugin<ISettings> : Plugin
     {
         internal static readonly ILogger logger = LogManager.GetLogger();
         internal static IResourceProvider resources = new ResourceProvider();
@@ -18,23 +41,15 @@ namespace CommonPluginsShared.PlayniteExtended
         public ISettings PluginSettings { get; set; }
 
         public string PluginFolder { get; set; }
-        public static TPluginDatabase PluginDatabase { get; set; }
 
 
-        public PluginExtended(IPlayniteAPI api, bool WithDatabase = false) : base(api)
+        protected PlaynitePlugin(IPlayniteAPI api) : base(api)
         {
             // Get plugin's settings 
             PluginSettings = typeof(ISettings).CrateInstance<ISettings>(this);
 
             // Get plugin's location 
             PluginFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-
-            // Get plugin's database if used
-            if (WithDatabase)
-            {
-                PluginDatabase = typeof(TPluginDatabase).CrateInstance<TPluginDatabase>(PlayniteApi, PluginSettings, this.GetPluginUserDataPath());
-                PluginDatabase.InitializeDatabase();
-            }
 
             LoadCommon();
         }
