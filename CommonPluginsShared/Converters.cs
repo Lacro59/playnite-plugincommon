9b6c83665283eb62e001996698cc1ede3ed8cb7b
Lacro59 <lacro59@gmail.com>
2021-05-27 10:24:57 +0200

    updated font & added new converter

--- CommonPluginsShared/Converters.cs
+++ CommonPluginsShared/Converters.cs
@@ -467,4 +467,31 @@ namespace CommonPluginsShared
             throw new NotImplementedException();
         }
     }
+
+
+    public class StringRemoveConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (value is string && parameter is string) 
+                {
+                    return ((string)value).Replace((string)parameter, string.Empty);
+                }
+            }
+            catch (Exception ex)
+            {
+                Common.LogError(ex, false);
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
