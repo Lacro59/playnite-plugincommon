e4fee6286cef7171b3ab1cacfca2547af639ce44
Lacro59 <lacro59@gmail.com>
2021-07-02 15:47:00 +0200

    updated SteamApi

--- CommonPluginsStores/SteamApi.cs
+++ CommonPluginsStores/SteamApi.cs
@@ -240,4 +240,26 @@ namespace CommonPluginsStores
         [SerializationPropertyName("name")]
         public string Name { get; set; }
     }
+
+    public class SteamAchievementData
+    {
+        [SerializationPropertyName("rawname")]
+        public string RawName { get; set; }
+        [SerializationPropertyName("hidden")]
+        public bool Hidden { get; set; }
+        [SerializationPropertyName("closed")]
+        public int Closed { get; set; }
+        [SerializationPropertyName("unlock_time")]
+        public int UnlockTime { get; set; }
+        [SerializationPropertyName("icon_closed")]
+        public string IconClosed { get; set; }
+        [SerializationPropertyName("icon_open")]
+        public string IconOpen { get; set; }
+        [SerializationPropertyName("progress")]
+        public dynamic Progress { get; set; }
+        [SerializationPropertyName("name")]
+        public string Name { get; set; }
+        [SerializationPropertyName("desc")]
+        public string Desc { get; set; }
+    }
 }
