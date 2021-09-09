7c70b184cb7b7176b79c5677997a2ee250449148
Lacro59 <lacro59@gmail.com>
2021-09-09 13:50:02 +0200

    added Commands

---
+++ CommonPluginsShared/Commands.cs.cs
@@ -0,0 +1,33 @@
+﻿using Playnite.SDK;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace CommonPluginsShared
+{
+    public static class Commands
+    {
+        private static ILogger logger = LogManager.GetLogger();
+
+        public static RelayCommand<object> RestartRequired
+        {
+            get => new RelayCommand<object>((sender) =>
+            {
+                try
+                {
+                    var WinParent = UI.FindParent<Window>((FrameworkElement)sender);
+
+                    if (WinParent.DataContext?.GetType().GetProperty("IsRestartRequired") != null)
+                    {
+                        ((dynamic)WinParent.DataContext).IsRestartRequired = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Common.LogError(ex, false);
+                }
+            });
+        }
+    }
+}
