52c20e84523d11844b80a4a5a1394165cefb8e50
Lacro59 <lacro59@gmail.com>
2021-09-28 15:59:48 +0200

    removed unused

--- CommonPluginsShared/Tools.cs
+++ CommonPluginsShared/Tools.cs
@@ -39,16 +39,6 @@ namespace CommonPluginsShared
         }
 
 
-        // TODO Used?
-        // https://stackoverflow.com/a/48735199
-        //public static JArray RemoveValue(JArray oldArray, dynamic obj)
-        //{
-        //    List<string> newArray = oldArray.ToObject<List<string>>();
-        //    newArray.Remove(obj);
-        //    return JArray.FromObject(newArray);
-        //}
-
-
         #region Date manipulations
         /// <summary>
         /// Get number week from a DateTime
