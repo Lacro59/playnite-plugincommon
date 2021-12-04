35b81b3e709c828d9876049d712c70ff05e199cf
Lacro59 <lacro59@gmail.com>
2021-12-04 20:27:05 +0100

    [Bug] Illegal characters in file and folder names #27

---
+++ CommonPluginsShared/Paths.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonPluginsShared
+{
+    public class Paths
+    {
+        public static string GetSafePath(string path)
+        {
+            string pathReturn = string.Empty;
+            string[] PathFolders = path.Split('\\');
+            foreach (string folder in PathFolders)
+            {
+                if (pathReturn.IsNullOrEmpty())
+                {
+                    pathReturn += folder;
+                }
+                else
+                {
+                    pathReturn += "\\" + CommonPlayniteShared.Common.Paths.GetSafePathName(folder);
+                }
+            }
+
+            return pathReturn;
+        }
+    }
+}
