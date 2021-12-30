6b3426109a969783c824b09d0daab4b7ef8064c9
Lacro59 <lacro59@gmail.com>
2021-12-30 11:46:38 +0100

    improvements

--- CommonPluginsShared/Models/Folder.cs
+++ CommonPluginsShared/Models/Folder.cs
@@ -7,5 +7,9 @@ namespace CommonPluginsShared.Models
     public class Folder
     {
         public string FolderPath { get; set; }
+
+        // Specific SteamEmulator
+        public string GameId { get; set; }
+        public bool HasGame { get; set; }
     }
 }
