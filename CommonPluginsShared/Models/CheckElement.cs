7b90d769fcd07780c87e6f25ce038ece4af5bd21
Lacro59 <lacro59@gmail.com>
2022-11-09 10:51:03 +0100

    add new model CheckElement

---
+++ CommonPluginsShared/Models/CheckElement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonPluginsShared.Models
+{
+    public class CheckElement
+    {
+        public string Icon { get; set; }
+        public string Name { get; set; }
+        public string NameShort { get; set; }
+        public bool IsCheck { get; set; }
+    }
+}
