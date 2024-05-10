52389d548d0a5ceccf492c33a9128ee5d4ab23f7
Lacro59 <lacro59@gmail.com>
2024-05-10 13:10:29 +0200

    code optimizations

--- CommonPluginsShared/Converters/ValueOperationConverter.cs
+++ CommonPluginsShared/Converters/ValueOperationConverter.cs
@@ -6,25 +6,17 @@ using System.Windows.Data;
 
 namespace CommonPluginsShared.Converters
 {
-    class ValueOperationConverter : IMultiValueConverter
+    internal class ValueOperationConverter : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             try
             {
-                if (parameter.ToString() == "-")
-                {
-                    return double.Parse(values[0].ToString()) - double.Parse(values[1].ToString());
-                }
-                else
-                {
-                    return double.Parse(values[0].ToString()) + double.Parse(values[1].ToString());
-                }
+                return parameter.ToString() == "-"
+                    ? double.Parse(values[0].ToString()) - double.Parse(values[1].ToString())
+                    : (object)(double.Parse(values[0].ToString()) + double.Parse(values[1].ToString()));
             }
-            catch
-            {
-                
-            }
+            catch { }
 
             return double.NaN;
         }
