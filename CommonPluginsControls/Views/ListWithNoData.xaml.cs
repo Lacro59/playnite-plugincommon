4672a80ac3f29032a9c8534e51c64d6c3df710c9
Lacro59 <lacro59@gmail.com>
2024-10-30 09:26:38 +0100

    code optimizations

--- CommonPluginsControls/Views/ListWithNoData.xaml.cs
+++ CommonPluginsControls/Views/ListWithNoData.xaml.cs
@@ -1,21 +1,11 @@
 ﻿using CommonPluginsShared.Interfaces;
 using Playnite.SDK;
-using Playnite.SDK.Data;
 using Playnite.SDK.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CommonPluginsControls.Views
 {
@@ -27,7 +17,7 @@ namespace CommonPluginsControls.Views
         private IPluginDatabase PluginDatabase { get; set; }
         private List<GameData> gameData = new List<GameData>();
 
-        RelayCommand<Guid> GoToGame { get; set; }
+        private RelayCommand<Guid> GoToGame { get; set; }
 
         public ListWithNoData(IPluginDatabase PluginDatabase)
         {
