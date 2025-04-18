1dc6c6e3547b0c2403306dce70bd13804db07165
Lacro59 <lacro59@gmail.com>
2025-04-18 17:15:00 +0200

    Update AccountInfos.cs

--- CommonPluginsStores/Models/AccountInfos.cs
+++ CommonPluginsStores/Models/AccountInfos.cs
@@ -7,22 +7,30 @@ namespace CommonPluginsStores.Models
 {
     public class AccountInfos : ObservableObject
     {
-        public string UserId { get; set; }
-        public string ClientId { get; set; }
-        public string Pseudo { get; set; }
-        public string Avatar { get; set; }
-        public string Link { get; set; }
+        private string _userId;
+        public string UserId { get => _userId; set => SetValue(ref _userId, value); }
+
+        private string _clientId;
+        public string ClientId { get => _clientId; set => SetValue(ref _clientId, value); }
+
+        private string _pseudo;
+        public string Pseudo { get => _pseudo; set => SetValue(ref _pseudo, value); }
+
+        private string _avatar;
+        public string Avatar { get => _avatar; set => SetValue(ref _avatar, value); }
+
+        private string _link;
+        public string Link { get => _link; set => SetValue(ref _link, value); }
 
         public DateTime? DateAdded { get; set; }
-
 
         public bool IsCurrent { get; set; } = false;
         [DontSerialize]
         public bool IsPrivate { get; set; } = true;
 
-        private AccountStatus accountStatus = AccountStatus.Checking;
+        private AccountStatus _accountStatus = AccountStatus.Checking;
         [DontSerialize]
-        public AccountStatus AccountStatus { get => accountStatus; set => SetValue(ref accountStatus, value); }
+        public AccountStatus AccountStatus { get => _accountStatus; set => SetValue(ref _accountStatus, value); }
 
         public string ApiKey { get; set; }
 
