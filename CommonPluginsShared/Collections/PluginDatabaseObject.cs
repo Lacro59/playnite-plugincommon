701f6fe7b9e899be260bdb960424bb760c484156
Lacro59 <lacro59@gmail.com>
2025-10-16 16:17:47 +0200

    Add LoadMoreData hook to PluginDatabaseObject
    Introduced a new internal virtual method LoadMoreData in PluginDatabaseObject and invoked it during database initialization. This allows derived classes to load additional data as needed after the main data load.

--- CommonPluginsShared/Collections/PluginDatabaseObject.cs
+++ CommonPluginsShared/Collections/PluginDatabaseObject.cs
@@ -52,9 +52,6 @@ namespace CommonPluginsShared.Collections
         private IEnumerable<Guid> PreviousIds { get; set; } = new List<Guid>();
 
 
-
-
-
         /// <summary>
         /// Constructor for PluginDatabaseObject.
         /// </summary>
@@ -81,7 +78,6 @@ namespace CommonPluginsShared.Collections
             API.Instance.Database.Games.ItemUpdated += Games_ItemUpdated;
             API.Instance.Database.Games.ItemCollectionChanged += Games_ItemCollectionChanged;
         }
-
 
         #region Database Initialization and Management
 
@@ -132,6 +128,9 @@ namespace CommonPluginsShared.Collections
                 // Delete game deleted in Playnite
                 DeleteDataWithDeletedGame();
 
+                // Load additional data if necessary
+                LoadMoreData();
+
                 stopWatch.Stop();
 
                 // Log the elapsed time and number of items loaded
@@ -150,6 +149,11 @@ namespace CommonPluginsShared.Collections
                 Common.LogError(ex, false, true, PluginName);
                 return false;
             }
+        }
+
+        internal virtual void LoadMoreData()
+        {
+
         }
 
         /// <summary>
@@ -333,7 +337,6 @@ namespace CommonPluginsShared.Collections
         }
 
         #endregion
-
 
         #region Database Item Methods
 
