11f7758e539d76b14d8534369483e714976ffb9d
Lacro59 <lacro59@gmail.com>
2026-01-19 08:41:42 +0100

    Update PluginDataBaseGameDetails.cs

--- CommonPluginsShared/Collections/PluginDataBaseGameDetails.cs
+++ CommonPluginsShared/Collections/PluginDataBaseGameDetails.cs
@@ -1,7 +1,26 @@
-﻿namespace CommonPluginsShared.Collections
+﻿using System.Collections.Generic;
+
+namespace CommonPluginsShared.Collections
 {
-    public abstract class PluginDataBaseGameDetails<T, Y> : PluginDataBaseGame<T>
-    {
-        public abstract Y ItemsDetails { get; set; }
-    }
+	public abstract class PluginDataBaseGameDetails<T, Y> : PluginDataBaseGame<T>
+	{
+		private Y itemsDetails = default;
+
+		/// <summary>
+		/// Gets or sets the list of itemsDetails associated with the game.
+		/// Setting this property triggers the OnItemsChanged event and refreshes cached values.
+		/// </summary>
+		public Y ItemsDetails
+		{
+			get
+			{
+				return itemsDetails == null ? default : itemsDetails;
+			}
+			set
+			{
+				SetValue(ref itemsDetails, value);
+				OnItemsChanged();
+			}
+		}
+	}
 }
