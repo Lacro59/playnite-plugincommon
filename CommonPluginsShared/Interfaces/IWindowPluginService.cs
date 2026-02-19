18a6ce72891dd1ba2a15c7f2ee51371154b466ec
Lacro59 <lacro59@gmail.com>
2026-02-19 17:56:41 +0100

    Update XAML files for design-time properties and enhance IWindowPluginService interface
    - Modified ListDataWithoutGame.xaml and TransfertData.xaml to include design-time height and width attributes for improved visual layout in design view.
    - Expanded IWindowPluginService interface with new methods for showing plugin data windows and handling data mismatches, enhancing functionality and maintainability.

--- CommonPluginsShared/Interfaces/IWindowPluginService.cs
+++ CommonPluginsShared/Interfaces/IWindowPluginService.cs
@@ -1,6 +1,9 @@
 using CommonPluginsShared.Interfaces;
+using CommonPluginsShared.Models;
 using Playnite.SDK;
 using Playnite.SDK.Models;
+using Playnite.SDK.Plugins;
+using System.Collections.Generic;
 
 namespace CommonPluginsShared.Services
 {
@@ -13,8 +16,18 @@ namespace CommonPluginsShared.Services
 
 		IPluginDatabase PluginDatabase { get; }
 
+		void ShowPluginGameDataWindow(GenericPlugin plugin, Game gameContext);
+
+		void ShowPluginGameDataWindow(GenericPlugin plugin);
+
 		void ShowPluginGameDataWindow(Game gameContext);
 
 		void ShowPluginGameNoDataWindow();
+
+		void ShowPluginDataMismatch();
+
+		void ShowPluginTransfertData(IEnumerable<DataGame> dataGames);
+
+		void ShowPluginDataWithoutGame(IEnumerable<DataGame> dataGames);
 	}
 }
